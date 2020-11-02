31056e4fbc3d4ba78739e19060bce5d36f510450
Leo Singer <leo.singer@ligo.org>
2020-11-02 12:28:41 -0500

    Disable hypothesis deadlines

--- dorado/sensitivity/tests/test_sensitivity.py
+++ dorado/sensitivity/tests/test_sensitivity.py
@@ -1,6 +1,6 @@
 from astropy.coordinates import SkyCoord
 from astropy import units as u
-from hypothesis import given
+from hypothesis import given, settings
 from hypothesis.strategies import booleans, floats, just
 from synphot import ConstFlux1D, SourceSpectrum
 from pytest import approx
@@ -15,6 +15,7 @@ from .. import get_limmag, get_snr
     dec=just(90),
     night=booleans(),
     bandpass=just('D1'))
+@settings(deadline=None)
 def test_round_trip(snr, exptime, ra, dec, night, bandpass):
     """Test round trip: get_snr(get_limmag(...))"""
     kwargs = dict(
