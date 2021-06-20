Fix broadcasting over night argument

dorado/sensitivity/__init__.py
@@ -26,9 +26,11 @@ def _get_count_rate(source_spectrum):
 
 def _get_background_count_rate(coord, time, night):
     zodi_scale = backgrounds.get_zodiacal_light_scale(coord, time)
+    night = np.asarray(night)
     return np.square(constants.PLATE_SCALE.to_value(u.arcsec / u.pix)) * (
         _get_count_rate(backgrounds.high_zodiacal_light) * zodi_scale +
-        _get_count_rate(backgrounds.get_airglow(night))
+        _get_count_rate(backgrounds.get_airglow(night[..., np.newaxis])) +
+        _get_count_rate(backgrounds.get_galactic(coord[..., np.newaxis]))
     )
 
 

dorado/sensitivity/backgrounds.py
@@ -9,11 +9,12 @@
 from importlib import resources
 
 from astropy.coordinates import GeocentricTrueEcliptic, get_sun, SkyCoord
+from astropy.stats.funcs import gaussian_fwhm_to_sigma
 from astropy.table import QTable
 from astropy import units as u
 import numpy as np
 from scipy.interpolate import RegularGridInterpolator
-from synphot import Empirical1D, GaussianFlux1D, PowerLawFlux1D, SourceSpectrum
+from synphot import Empirical1D, Gaussian1D, PowerLawFlux1D, SourceSpectrum
 from synphot.units import PHOTLAM
 
 from . import data
@@ -132,10 +133,16 @@ def get_airglow(night):
 
     """
     flux = np.where(night, 1.5e-17, 1.5e-15) * u.erg * u.s**-1 * u.cm**-2
-    return SourceSpectrum(GaussianFlux1D,
-                          mean=2471 * u.angstrom,
-                          fwhm=0.023 * u.angstrom,
-                          total_flux=flux)
+    x_0 = 2471 * u.angstrom
+    fwhm = 0.023 * u.angstrom
+    stdev = fwhm * gaussian_fwhm_to_sigma
+    # FIXME: use Gaussian1D instead of GaussianFlux1D
+    # because GaussianFlux1D does not broadcast properly.
+    return SourceSpectrum(
+        Gaussian1D,
+        mean=x_0,
+        stddev=stdev,
+        amplitude=flux / (np.sqrt(2 * np.pi) * stdev))
 
 
 def get_galactic(coord):